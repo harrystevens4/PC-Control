aa03695f72e10c00edf65e7be5218feb857db634
harry <harrywstevens4@gmail.com>
2026-07-11 18:29:46 +0100

    switched main activity to java

---
+++ app/src/main/java/com/example/pccontrol/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.pccontrol;
+
+import android.app.Activity;
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    private static Activity app;
+    @Override
+    public void onCreate(Bundle saved_instance_state){
+        super.onCreate(saved_instance_state);
+        MainActivity.app = this;
+        //setup notification channel
+        NotificationChannel notification_channel = new NotificationChannel("ErrorChannel","Errors", NotificationManager.IMPORTANCE_HIGH);
+        notification_channel.enableVibration(true);
+        notification_channel.enableLights(true);
+        NotificationManager notification_manager = this.getSystemService(NotificationManager.class);
+        notification_manager.createNotificationChannel(notification_channel);
+    }
+    public static Context getAppContext(){
+        return app.getApplicationContext();
+    }
+}
